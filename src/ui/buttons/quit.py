a6a0d5eff74bf993dbe25f81e7f0762fc39398eb
laurelcrelia <laurel.matt@helsinki.fi>
2023-03-26 23:07:13 +0300

    Create buttons for menu view

---
+++ src/ui/buttons/quit.py
@@ -0,0 +1,33 @@
+import pygame, sys
+
+class Quit():
+    def __init__(self, screen):
+
+        self.text_pos = [180, 240] #Quit-button's text position in the width and height directions of the window
+        self.box = [100, 40] #Button's box width and height
+        self.border= [10, 5] #Button's border width and height
+        self.font = pygame.font.SysFont("Segoe UI", 30)
+        self.text = self.font.render("Quit", True, (205, 38, 38))
+        self.screen = screen
+
+    def draw(self):
+        self.screen.blit(self.text, (self.text_pos[0], self.text_pos[1]))
+
+    def update(self):
+        mouse = pygame.mouse.get_pos()
+        if self.text_pos[0]-self.border[0] <= mouse[0] <= self.text_pos[0]+(self.box[0]-self.border[0]) and self.text_pos[1]-self.border[1] <= mouse[1] <= self.text_pos[1]+(self.box[1]-self.border[1]):
+            pygame.draw.rect(self.screen, (190, 190, 190),
+                             [self.text_pos[0]-self.border[0], self.text_pos[1]-self.border[1], self.box[0], self.box[1]])
+        else:
+            pygame.draw.rect(self.screen, (100, 100, 100),
+                             [self.text_pos[0]-self.border[0], self.text_pos[1]-self.border[1], self.box[0], self.box[1]])
+
+    def handle_event(self, event):
+        pygame.init()
+        mouse = pygame.mouse.get_pos()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.text_pos[0]-self.border[0] <= mouse[0] <= self.text_pos[0]+(self.box[0]-self.border[0]) and self.text_pos[1]-self.border[1] <= mouse[1] <= self.text_pos[1]+(self.box[1]-self.border[1]):
+                pygame.quit()
+                sys.exit()
